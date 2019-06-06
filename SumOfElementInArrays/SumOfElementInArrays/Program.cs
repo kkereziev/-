5d6b8722cf2c2539ee7bb592aff15e59e6e10a3f
krumeto97 <krumeto97@abv.bg>
2019-06-06 22:36:19 +0300

    Add files via upload

---
+++ SumOfElementInArrays/SumOfElementInArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfElementInArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] arrayTwo = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int sum=SumOfArrayElements(arrayOne,arrayTwo);
+            Console.WriteLine(sum);
+        }
+
+        static int SumOfArrayElements(int[] arrayOne, int[] arrayTwo)
+        {
+            int sum = 0;
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                sum += arrayOne[i] + arrayTwo[i];
+            }
+            return sum;
+        }
+    }
+}
