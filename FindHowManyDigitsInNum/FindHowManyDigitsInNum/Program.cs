e1476fac7b73c3695522f3a87e1a129f8f8bc11d
krumeto97 <krumeto97@abv.bg>
2019-06-06 22:22:08 +0300

    Add files via upload

---
+++ FindHowManyDigitsInNum/FindHowManyDigitsInNum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FindHowManyDigitsInNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            FindHowManyDigits(num);
+        }
+
+        static void FindHowManyDigits(int num)
+        {
+            int count = 0;
+            while (num>0)
+            {
+                num /= 10;
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
