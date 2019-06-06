9aed36a143b2bd9bc4a796dbff1ee5ccee78d8d4
krumeto97 <krumeto97@abv.bg>
2019-06-06 22:37:16 +0300

    Add files via upload

---
+++ Gradation/Gradation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Gradation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            int result=Gradation(num, power);
+            Console.WriteLine(result);
+        }
+
+        static int Gradation(int num, int power)
+        {
+            int firstNum = num;
+            for (int i = 1; i < power; i++)
+            {
+                firstNum *= num;
+            }
+            return firstNum;
+        }
+    }
+}
