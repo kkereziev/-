0948356479bf3ce3bb4ac12a1aa3c4a72badbca0
krumeto97 <krumeto97@abv.bg>
2019-06-06 22:41:49 +0300

    Add files via upload

---
+++ Factoriel/Factoriel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Factoriel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int result=Factoriel(num);
+            Console.WriteLine(result);
+        }
+
+        static int Factoriel(int num)
+        {
+            int r = 1;
+            for (int i = 2; i <=num; i++)
+            {
+                r *= i;
+            }
+            return r;
+        }
+    }
+}
